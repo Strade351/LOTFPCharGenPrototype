7aa8db99a58dd7186e56aa9b87e13c7fb92431e1
velvetmoor <tolybondarev@gmail.com>
2017-11-22 18:52:06 +0400

    Controller class

--- src/Demo.java
+++ src/Demo.java
@@ -2,21 +2,8 @@ import java.io.IOException;
 
 public class Demo {
     public static void main(String[] args) throws IOException {
-        Character character = new Character();
-        Roller roller = new Roller(character);
-        roller.rollClass();
-        roller.rollSex();
-        roller.rollAttributes();
-        roller.rollHP();
-        roller.setSaves();
-        roller.setAB();
-        roller.setSkills();
-        roller.setSpells();
-        roller.rollSilver();
-        roller.rollEquipment();
-        roller.setAC();
-        character = roller.getCharacter();
-        System.out.println(character.toString());
-        ConfigReader configReader = new ConfigReader();
+        Controller controller = new Controller();
+        controller.rollCharacter();
+        controller.printCharacter();
     }
 }
